6f6e84449b678b4a9dcf576788c867fd7063e0ad
Andre Weissflog <floooh@gmail.com>
2016-02-18 19:50:06 +0100

    change vertex format to UByte4N and scale manually in VS for D3D11 compat

--- src/Geom.cc
+++ src/Geom.cc
@@ -17,8 +17,8 @@ Geom::Setup(const GfxSetup& gfxSetup, Id indexMesh, Id shd, const Shaders::Voxel
     // create Gfx resources
     auto meshSetup = MeshSetup::Empty(MaxNumVertices, Usage::Dynamic);
     meshSetup.Layout
-        .Add(VertexAttr::Position, VertexFormat::UByte4)
-        .Add(VertexAttr::Normal, VertexFormat::UByte4);
+        .Add(VertexAttr::Position, VertexFormat::UByte4N)
+        .Add(VertexAttr::Normal, VertexFormat::UByte4N);
     this->Mesh = Gfx::CreateResource(meshSetup);
     auto dss = DrawStateSetup::FromMeshAndShader(indexMesh, shd);
     dss.Meshes[1] = this->Mesh;
